f713ed6a71c6630544d223d4797eb336cf320b00
Moein Hosseini <moein.hoseini92@gmail.com>
2024-10-25 20:01:36 +0330

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,114 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
+  const [selection, setSelection] = useState<string>();
+  const [position, setPosition] = useState<Record<string, number>>();
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [newtext, setNextText] = useState<string>();
+
+  function onSelectEnd() {
+    const activeSelection = document.getSelection();
+    const text = activeSelection?.toString();
+
+    if (!activeSelection || !text) {
+      setSelection(undefined);
+      return;
+    }
+
+    setSelection(text);
+
+    const rect = activeSelection.getRangeAt(0).getBoundingClientRect();
+
+    setPosition({
+      x: rect.left + rect.width / 2 - 80 / 2,
+      y: rect.top + window.scrollY - 30,
+      width: rect.width,
+      height: rect.height,
+    });
+  }
+
+  function onSelectStart() {
+    setSelection(undefined);
+  }
+
+  useEffect(() => {
+    document.addEventListener("selectstart", onSelectStart);
+    document.addEventListener("mouseup", onSelectEnd);
+    return () => {
+      document.removeEventListener("selectstart", onSelectStart);
+
+      document.removeEventListener("mouseup", onSelectEnd);
+    };
+  }, []);
+
+  const sampleText = `Lorem ipsum, 123123 dolor sit amet consectetur adipisicing elit. Aliquam eaque
+  rem neque magnam odit qui, repudiandae repellat aut placeat culpa enim
+  incidunt recusandae natus! Autem, illum nobis! Voluptatem, officiis
+  similique.`;
+
+  function onShare() {
+    if (!selection) return;
+    const message = `answer is : ${selection}`;
+    const newAnswers = [...answers, selection];
+    setAnswers(newAnswers);
+    const questionObject = {
+      questionId: uuidv4(),
+      questionBody: message,
+      qustionAnswers: newAnswers,
+      options: null,
+    };
+    console.log(questionObject);
+    textToArray(sampleText, newAnswers);
+  }
+
+  function textToArray(text: string, newAnswers: string[]) {
+    const words = text
+      .replace(/\n\n*/g, "\r\n")
+      .replace(/^\s+|\s+$/gm, "")
+      .split(" ");
+
+    let answerNumber = 1;
+    const newText = words.map((t) => {
+      const findWord = newAnswers.includes(t);
+      if (findWord) {
+        t = `${answerNumber}-------`;
+        answerNumber = answerNumber + 1;
+      }
+      return t;
+    });
+    setNextText(newText.join(" "));
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <p>{sampleText}</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div role="dialog" aria-labelledby="share" aria-haspopup="dialog">
+        {selection && position && (
+          <p
+            className="
+            absolute -top-2 left-0 w-[180px] h-[30px] bg-black text-white rounded m-0
+            after:absolute after:top-full after:left-1/2 after:-translate-x-2 after:h-0 after:w-0 after:border-x-[6px] after:border-x-transparent after:border-b-[8px] after:border-b-black after:rotate-180
+          "
+            style={{
+              transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <button
+              className="flex w-full h-full justify-between items-center px-2 bg-gray-500 rounded-lg"
+              onClick={() => onShare()}
+            >
+              <span id="share" className="text-xs">
+                Select as answer
+              </span>
+            </button>
+          </p>
+        )}
+        <p>{newtext}</p>
+      </div>
     </div>
   );
 }
